e3c7b9d2ffb73af2e27a149b3819fd286ad54242
Alex James <captainjack@clara.co.uk>
2021-07-13 11:01:28 +0100

    Update build_win64.sh

--- build_win64.sh
+++ build_win64.sh
@@ -10,6 +10,7 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 mkdir -p $PREFIX
 cd build_win64
 
+# libusb
 if [[ ! -f $PREFIX/lib/libusb-1.0.a ]]; then
 	
 	if [[ ! -f libusb-1.0.22.tar.bz2 ]]; then
@@ -23,6 +24,7 @@ if [[ ! -f $PREFIX/lib/libusb-1.0.a ]]; then
 	cd ..
 fi
 
+# hackrf
 if [[ ! -f $PREFIX/lib/libhackrf.a ]]; then
 	
 	if [[ ! -f hackrf-2018.01.1.tar.gz ]]; then
@@ -33,7 +35,9 @@ if [[ ! -f $PREFIX/lib/libhackrf.a ]]; then
 	rm -rf hackrf-2018.01.1/host/libhackrf/build
 	mkdir -p hackrf-2018.01.1/host/libhackrf/build
 	cd hackrf-2018.01.1/host/libhackrf/build
-	mingw64-cmake \
+	cmake .. \
+		-DCMAKE_SYSTEM_NAME=Windows \
+		-DCMAKE_C_COMPILER=$HOST-gcc \
 		-DCMAKE_INSTALL_PREFIX=$PREFIX \
 		-DCMAKE_INSTALL_LIBPREFIX=$PREFIX/lib \
 		-DLIBUSB_INCLUDE_DIR=$PREFIX/include/libusb-1.0 \
@@ -44,6 +48,7 @@ if [[ ! -f $PREFIX/lib/libhackrf.a ]]; then
 	find $PREFIX -name libhackrf\*.dll\* -delete
 fi
 
+# osmo-fl2k
 if [[ ! -f $PREFIX/lib/libosmo-fl2k.a ]]; then
 	
 	if [[ ! -d osmo-fl2k ]]; then
@@ -53,7 +58,9 @@ if [[ ! -f $PREFIX/lib/libosmo-fl2k.a ]]; then
 	rm -rf osmo-fl2k/build
 	mkdir -p osmo-fl2k/build
 	cd osmo-fl2k/build
-	mingw64-cmake \
+	cmake .. \
+		-DCMAKE_SYSTEM_NAME=Windows \
+		-DCMAKE_C_COMPILER=$HOST-gcc \
 		-DCMAKE_INSTALL_PREFIX=$PREFIX \
 		-DCMAKE_INSTALL_LIBPREFIX=$PREFIX \
 		-DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
@@ -64,6 +71,7 @@ if [[ ! -f $PREFIX/lib/libosmo-fl2k.a ]]; then
 	mv $PREFIX/lib/liblibosmo-fl2k_static.a $PREFIX/lib/libosmo-fl2k.a
 fi
 
+# AAC codec
 if [[ ! -f $PREFIX/lib/libfdk-aac.a ]]; then
 	
 	if [[ ! -d fdk-aac ]]; then
@@ -77,19 +85,101 @@ if [[ ! -f $PREFIX/lib/libfdk-aac.a ]]; then
 	cd ..
 fi
 
+# opus codec
 if [[ ! -f $PREFIX/lib/libopus.a ]]; then
 	
-	if [[ ! -f opus-1.3.tar.gz ]]; then
-		wget https://archive.mozilla.org/pub/opus/opus-1.3.tar.gz
-		tar -xvzf opus-1.3.tar.gz
+	if [[ ! -f opus-1.3.1.tar.gz ]]; then
+		wget https://archive.mozilla.org/pub/opus/opus-1.3.1.tar.gz
+		tar -xvzf opus-1.3.1.tar.gz
 	fi
 	
-	cd opus-1.3
-	./configure --host=$HOST --prefix=$PREFIX --enable-static --disable-shared --disable-doc --disable-extra-programs
+	cd opus-1.3.1
+	./configure CFLAGS='-D_FORTIFY_SOURCE=0' --host=$HOST --prefix=$PREFIX --enable-static --disable-shared --disable-doc --disable-extra-programs
 	make -j4 install
 	cd ..
 fi
 
+# zlib, required for logo support
+if [[ ! -f $PREFIX/lib/libz.a ]]; then
+
+    if [[ ! -f zlib-1.2.11.tar.gz ]]; then
+        wget http://zlib.net/zlib-1.2.11.tar.gz
+        tar xzvf zlib-1.2.11.tar.gz
+    fi
+
+    cd zlib-1.2.11
+    CC=$HOST-gcc AR=$HOST-ar RANLIB=$HOST-ranlib \
+    ./configure --prefix=$PREFIX --static
+    make -j4 install
+    cd ..
+fi
+
+# freetype2, required for subtitles and timestamp
+if [[ ! -f $PREFIX/lib/libfreetype.a ]]; then
+
+    if [[ ! -f freetype-2.10.4.tar.gz ]]; then
+        wget https://download.savannah.gnu.org/releases/freetype/freetype-2.10.4.tar.gz
+        tar xzvf freetype-2.10.4.tar.gz
+    fi
+
+    cd freetype-2.10.4
+    ./configure --prefix=$PREFIX --disable-shared --with-pic --host=$HOST --without-zlib --with-png=no --with-harfbuzz=no
+    make -j4 install
+    cd ..
+fi
+
+# libpng, also required for logo support
+if [[ ! -f $PREFIX/lib/libpng16.a ]]; then
+
+    if [[ ! -f libpng-1.6.37.tar.gz ]]; then
+        wget https://download.sourceforge.net/libpng/libpng-1.6.37.tar.gz
+	    tar xzvf libpng-1.6.37.tar.gz
+    fi
+
+    cd libpng-1.6.37
+    CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" \
+    ./configure --prefix=$PREFIX --host=$HOST
+    make -j4 install
+    cd ..
+fi
+
+# libiconv, pre-requisite for zvbi
+if [[ ! -f $PREFIX/lib/libiconv.a ]]; then
+
+    if [[ ! -f libiconv-1.16.tar.gz ]]; then
+        wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.16.tar.gz
+	    tar xzvf libiconv-1.16.tar.gz
+    fi
+
+    cd libiconv-1.16
+    ./configure --prefix=$PREFIX --host=$HOST --enable-static --disable-shared
+    make -j4 install
+    cd ..
+fi
+
+# zvbi, required for handling teletext subtitles in transport streams
+if [[ ! -f $PREFIX/lib/libzvbi.a ]]; then
+
+    if [[ ! -f zvbi-0.2.35.tar.bz2 ]]; then
+        wget https://download.sourceforge.net/project/zapping/zvbi/0.2.35/zvbi-0.2.35.tar.bz2
+        tar xjvf zvbi-0.2.35.tar.bz2
+        # Can't be cross compiled in its default state, needs to be patched first
+        # This repo contains the patch files that we need
+        git clone https://github.com/rdp/ffmpeg-windows-build-helpers.git
+        cd zvbi-0.2.35
+        patch -p0 < ../ffmpeg-windows-build-helpers/patches/zvbi-win32.patch
+        patch < ../ffmpeg-windows-build-helpers/patches/zvbi-no-contrib.diff
+    fi
+
+    CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" \
+    ./configure \
+        --prefix=$PREFIX --host=$HOST --enable-static --disable-shared --disable-dvb \
+        --disable-bktr --disable-proxy --disable-nls --without-doxygen --without-libiconv-prefix 
+    make -j4 install
+    cd ..
+fi
+
+# ffmpeg
 if [[ ! -f $PREFIX/lib/libavformat.a ]]; then
 	
 	if [[ ! -d ffmpeg ]]; then
@@ -99,8 +189,8 @@ if [[ ! -f $PREFIX/lib/libavformat.a ]]; then
 	cd ffmpeg
 	./configure \
 		--enable-gpl --enable-nonfree --enable-libfdk-aac --enable-libopus \
-		--enable-static --disable-shared --disable-programs \
-		--disable-outdevs --disable-encoders \
+		--enable-static --disable-shared --disable-programs --enable-zlib \
+		--enable-libfreetype --enable-libzvbi --disable-outdevs --disable-encoders \
 		--arch=x86_64 --target-os=mingw64 --cross-prefix=$HOST- \
 		--pkg-config=pkg-config --prefix=$PREFIX
 	make -j4 install
@@ -113,4 +203,3 @@ mv -f hacktv hacktv.exe || true
 $HOST-strip hacktv.exe
 
 echo "Done"
-
