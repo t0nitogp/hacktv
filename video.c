562dee13ca764fc39c74132dc16c7a0d22d6624d
Alex James <captainjack@clara.co.uk>
2021-07-27 21:04:17 +0100

    Upstream merge (#45)
    * Remove unneeded variable
    
    * Limit SECAM FM deviation
    
    * Add band reject filter designer
    
    * Add a luminance notch filter for SECAM
    
    * Clip int16 IIR filter output range
    
    Co-authored-by: Philip Heron <phil@sanslogic.co.uk>

--- video.c
+++ video.c
@@ -2662,7 +2662,7 @@ static int _vid_next_line_raster(vid_t *s, void *arg, int nlines, vid_line_t **l
 	   (seq[2] == 'a' || seq[3] == 'a'))
 	{
 		const cint16_t *g;
-		int16_t cs;
+		int16_t dmin, dmax;
 		
 		for(x = 0; x < s->width; x++)
 		{
@@ -2684,8 +2684,19 @@ static int _vid_next_line_raster(vid_t *s, void *arg, int nlines, vid_line_t **l
 		}
 		
 		x = s->fm_secam_fir.ntaps / 2;
+		fir_int16_process(&s->secam_l_fir, l->output, l->output + x * 2, s->width - x);
 		fir_int16_process(&s->fm_secam_fir, l->output + 1, l->output + 1 + x * 2, s->width - x);
 		iir_int16_process(&s->fm_secam_iir, l->output + 1, l->output + 1, s->width, 2);
+		
+		/* Limit the FM deviation */
+		dmin = s->fm_secam_dmin[((l->frame * s->conf.lines) + l->line) & 1];
+		dmax = s->fm_secam_dmax[((l->frame * s->conf.lines) + l->line) & 1];
+		
+		for(x = 0; x < s->width; x++)
+		{
+			if(l->output[x * 2 + 1] < dmin) l->output[x * 2 + 1] = dmin;
+			else if(l->output[x * 2 + 1] > dmax) l->output[x * 2 + 1] = dmax;
+		}
 		
 		x = s->active_left;
 		w = x + s->active_width;
@@ -3287,6 +3298,15 @@ int vid_init(vid_t *s, unsigned int sample_rate, const vid_config_t * const conf
 		fir_int16_low_pass(taps, 51, s->sample_rate, 1.50e6, 0.50e6, 1.0);
 		fir_int16_init(&s->fm_secam_fir, taps, 51);
 		
+		fir_int16_band_reject(taps, 51, s->sample_rate, SECAM_FM_FREQ - 1.0e6, SECAM_FM_FREQ + 1e6, 1e6, 1.0);
+		fir_int16_init(&s->secam_l_fir, taps, 51);
+		
+		/* FM deviation limits */
+		s->fm_secam_dmin[0] = lround((SECAM_CB_FREQ - SECAM_FM_FREQ - 350e3) / SECAM_FM_DEV * INT16_MAX);
+		s->fm_secam_dmax[0] = lround((SECAM_CB_FREQ - SECAM_FM_FREQ + 506e3) / SECAM_FM_DEV * INT16_MAX);
+		s->fm_secam_dmin[1] = lround((SECAM_CR_FREQ - SECAM_FM_FREQ - 506e3) / SECAM_FM_DEV * INT16_MAX);
+		s->fm_secam_dmax[1] = lround((SECAM_CR_FREQ - SECAM_FM_FREQ + 350e3) / SECAM_FM_DEV * INT16_MAX);
+		
 		s->fm_secam_bell = malloc(sizeof(cint16_t) * UINT16_MAX);
 		if(!s->fm_secam_bell)
 		{
@@ -3810,6 +3830,7 @@ void vid_free(vid_t *s)
 	/* Free allocated memory */
 	free(s->yiq_level_lookup);
 	free(s->colour_lookup);
+	fir_int16_free(&s->secam_l_fir);
 	fir_int16_free(&s->fm_secam_fir);
 	iir_int16_free(&s->fm_secam_iir);
 	_free_fm_modulator(&s->fm_secam);
